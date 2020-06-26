fa07aeee056ee9d9a8cb8584529eb3e94d262cc1
ruanres <ruan.silveira@ccc.ufcg.edu.br>
2020-06-26 12:23:18 -0300

    make search request

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import SearchBar from '../components/SearchBar';
+import useResults from '../hooks/useResults';
 
-const SearchScreen = () => (
-  <View>
-    <Text>Search screen</Text>
-  </View>
-);
+const SearchScreen = () => {
+  const [term, setTerm] = useState('');
+  const [searchApi, results, errorMessage] = useResults();
+
+  return (
+    <View>
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+      />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>
+        {`We have found ${results.length} results!`}
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
 
 export default SearchScreen;
