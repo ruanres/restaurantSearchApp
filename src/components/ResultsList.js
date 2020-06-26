d74371c4a10e87f7399de7d44278595013887cdc
ruanres <ruan.silveira@ccc.ufcg.edu.br>
2020-06-26 15:43:53 -0300

    create ResultsDetail component

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -2,23 +2,30 @@ import React from 'react';
 import {
   View, Text, StyleSheet, FlatList,
 } from 'react-native';
+import ResultsDetail from './ResultsDetail';
 
 const ResultsList = ({ title, results }) => (
-  <View>
+  <View style={styles.container}>
     <Text style={styles.title}>{title}</Text>
     <FlatList
       data={results}
       keyExtractor={(result) => result.id}
-      renderItem={({ item }) => <Text>{item.name}</Text>}
+      renderItem={({ item }) => <ResultsDetail result={item} />}
+      showsHorizontalScrollIndicator={false}
       horizontal
     />
   </View>
 );
 
 const styles = StyleSheet.create({
+  container: {
+    marginBottom: 10,
+  },
   title: {
     fontSize: 18,
     fontWeight: 'bold',
+    marginLeft: 15,
+    marginBottom: 5,
   },
 });
 
